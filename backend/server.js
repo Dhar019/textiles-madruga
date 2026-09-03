01bc34d0701cf8c1157636644dd7d93c519755d0
Dhar19 <eldani000219@gmail.com>
2026-09-02 20:36:26 -0400

    Eliminadas dependencias innecesarias para acelerar build

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,13 @@
 
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(helmet());
+//app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
